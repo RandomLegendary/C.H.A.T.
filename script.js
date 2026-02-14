bfe1d1566228c0baeba0c779b822632fa6dfe846
RandomLegendary <aidan.arijs2@gmail.com>
2026-02-14 14:31:58 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputBox = document.getElementById('message')
+const submitBtn = document.getElementById('submit')
+const divMessages = document.getElementById('messages')
+
+submitBtn.addEventListener('click', async function(e) {
+    e.preventDefault()
+
+    
+    fetch("http://localhost:8080/messages", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain"
+        },
+        body: `${inputBox.value}`
+    })
+    .then(res => res.text())
+    // .then(data => console.log(data));
+    
+    inputBox.value = ''
+
+    requestMessages()
+})
+
+window.addEventListener('load', function() {
+    requestMessages()
+})
+
+async function requestMessages() {
+    const result = await fetch("http://localhost:8080/messages")
+    const data = await result.json()
+
+    divMessages.innerHTML = ''
+
+    data.forEach(el => {
+        const pEl = document.createElement('p')
+        
+        pEl.innerHTML = el
+
+        divMessages.appendChild(pEl)
+
+    });
+
+    // console.log(data)
+}
+
+
+const clearBtn = document.getElementById('clear').addEventListener('click', async function clearMessages() {
+        await fetch("http://localhost:8080/clearList")
+        requestMessages()
+})
---
+++ spring-boot/src/main/java/com/aidan/springboot/Application.java
@@ -0,0 +1,36 @@
+package com.aidan.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+@CrossOrigin(origins = "http://localhost:5500")
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    List<String> messages = new ArrayList<>();
+    @PostMapping("/messages")
+    public String addMessage(@RequestBody String message) {
+        messages.add(message);
+        return message;
+    }
+
+    @GetMapping("/messages")
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    @GetMapping("/clearList")
+    public void clearList() {
+        messages.clear();
+    }
+
+}
