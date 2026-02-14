e151a6f5a36a42bb302bb3bf19254afb02eef97e
RandomLegendary <aidan.arijs2@gmail.com>
2026-02-14 20:24:36 +0100

    .

--- spring-boot/src/main/java/com/aidan/springboot/Application.java
+++ spring-boot/src/main/java/com/aidan/springboot/Application.java
@@ -7,18 +7,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-@CrossOrigin(origins = "https://chat2-oie2.onrender.com")
+@CrossOrigin(origins = "https://randomlegendary.github.io/")
 @SpringBootApplication
 @RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-        String port = System.getenv("PORT");
-        if (port == null || port.isEmpty()) {
-            port = "8080"; // default for local dev
-        }
     }
 
     List<String> messages = new ArrayList<>();
